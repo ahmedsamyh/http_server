e1d4d5b09ed30abdc4e639eb5985ad8261dbe1f1
momoyonwork <ahmedsamyhwork@gmail.com>
2024-12-25 22:02:57 +0500

    Create server

---
+++ index.ts
@@ -0,0 +1,31 @@
+import * as http from 'http';
+
+const PORT: number = 6969;
+const host: string = "127.0.0.1";
+
+const options = {};
+
+const server = http.createServer(options, (req, res) => {
+	console.log(`[${req.method}] ${req.url}`);
+	//console.log(`Got request from ${req.headers.host} accepting only: ${JSON.stringify(req.headers.accept)}`);
+
+	if (req.headers.accept && req.headers.accept.search("text/html") != -1) {
+		if (typeof req.url === "string") {
+			const url = new URL(`http://${process.env.HOST ?? 'localhost'}${req.url}`);
+			const pathname = url.pathname;
+			console.log(`Fetching ${pathname}`);
+
+
+			if (pathname == "/") {
+				res.writeHead(200, { 'content-type': 'text/html' });
+				res.write('<h1>Hello from HTTP Server</h1>');
+				res.end();
+			}
+		}
+	}
+});
+
+console.log(`Created server!`);
+
+server.listen(PORT, host);
+console.log(`Started listening on http://${host}:${PORT}`);
